ff038f05a67b7c1dc9e43b4fdd3a6daa3fd15f69
BrentMRPalmer <bpalm015@uottawa.ca>
2022-11-18 18:41:06 -0500

    includes message

--- app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
+++ app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ProductTest {
+
+    @Test
+    public void checkProductPrice() {
+        // I am making this test to fail
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the price of the product", "180.0", String.valueOf(aProduct.getPrice()));
+    }
     @Test
     public void checkProductName() {
         Product aProduct = new Product("1", "DELL MONITOR", 180);
@@ -15,13 +22,6 @@ public class ProductTest {
     public void checkProductId() {
         Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals("Check the id of the product", "1", aProduct.getId());
-    }
-
-    @Test
-    public void checkProductPrice() {
-        // I am making this test to fail
-        Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "180", String.valueOf(aProduct.getPrice()));
     }
 
 }
